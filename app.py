3821fd54d7d1a6bb3670d7c631fade342c7e622f
muhammad-talha-ijlal <muhammad.talha.ijlal@gmail.com>
2025-06-05 16:55:39 +0500

    update to comply with gunicorn

--- app.py
+++ app.py
@@ -61,9 +61,10 @@ def create_app(config_class=Config):
     
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
     # Create application instance
-    app = create_app()
     
     # Run the application
     app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'])
